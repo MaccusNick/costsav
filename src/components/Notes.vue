9245892c8daff4346855da1d750c46dca28e7149
NickZhang <maccus0329@gmail.com>
2021-11-16 21:40:05 +0800

    未使用v-model实现Note功能版本

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <label class="notes">
+      {{ value }}
       <span class="name">备注</span>
-      <input class="value" type="text" placeholder="在这里输入备注" />
+      <input
+        class="value"
+        type="text"
+        placeholder="在这里输入备注"
+        :value="value"
+        @input="onInput"
+      />
     </label>
   </div>
 </template>
 
-<script>
-export default {
-  name: "Notes",
-};
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Notes extends Vue {
+  value = "";
+
+  onInput(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
